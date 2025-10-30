9660815dec25da336e6829cecb6cd42ca449b390
Drozdi <drozdi.klim@gmail.com>
2025-10-30 12:34:46 +0300

    -

--- src/shared/utils/Axios-Interceptor.ts
+++ src/shared/utils/Axios-Interceptor.ts
@@ -1,5 +1,32 @@
 import axios from 'axios'
 
+interface IAxiosInterceptorDeafult {
+	message401?: string
+	accessToken?: string
+	refreshToken?: string
+	accessTokenKey?: string
+	refreshTokenKey?: string
+	urlRefreshToken?: string | Function
+}
+
+interface IAxiosInterceptorConfig extends IAxiosInterceptorDeafult, CreateAxiosDefaults {
+	handleRequest?: Function
+	handleResponse?: Function
+}
+
+interface IAxiosInterceptor extends Axios, IAxiosInterceptorDeafult {
+	axiosInstance: AxiosInstance
+	isRefreshing: boolean
+	refreshSubscribers: ((accessToken: string) => void)[]
+	// post: Function
+	// get: Function
+	// patch: Function
+	// delete: Function
+	// head: Function
+	// options: Function
+	// put: Function
+}
+
 export class AxiosInterceptor implements IAxiosInterceptor {
 	axiosInstance: AxiosInstance
 	isRefreshing: boolean = false
