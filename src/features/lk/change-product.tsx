fa941fc997bfe3542c068d4baccef48a7168adda
Drozdi <drozdi.klim@gmail.com>
2026-05-08 14:59:21 +0300

    -

--- src/features/lk/change-product.tsx
+++ src/features/lk/change-product.tsx
@@ -1,21 +1,23 @@
 import { useStoreUserProfile } from "@/entites/auth";
-import { SelectProductions, useQueryProductions } from "@/entites/users";
+import { SelectProductions } from "@/entites/users";
+import { useShallow } from "zustand/shallow";
 
 export const ChangeProduct = () => {
-	const storeUserProfile = useStoreUserProfile();
-	const qpl = useQueryProductions();
+	const { userInfo, production_id, setProductionId } = useStoreUserProfile(useShallow(state => ({
+		userInfo: state.userInfo,
+		production_id: state.production_id,
+		setProductionId: state.setProductionId,
+	})));
 
 	const handleChange = (value: string) => {
-		storeUserProfile.setProductionId(Number(value));
+		setProductionId(Number(value));
 	};
 
-	return (
-			<SelectProductions
-					allowDeselect={false}
-					excludeds={storeUserProfile.userInfo?.is_superuser ? [] : ["0"]}
-					variant="underline"
-					value={String(storeUserProfile.production_id)}
-					onChange={(value) => handleChange(value as string)}
-				/>
-	);
+	return <SelectProductions
+		allowDeselect={false}
+		excludeds={userInfo?.is_superuser ? [] : ["0"]}
+		variant="underline"
+		value={String(production_id)}
+		onChange={(value) => handleChange(value as string)}
+	/>
 };
