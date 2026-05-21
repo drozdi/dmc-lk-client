2bed564f3683adda7ace3f965943a59d509567ef
Drozdi <drozdi.klim@gmail.com>
2026-05-21 10:29:14 +0300

    -

--- src/features/labels/lables-history.tsx
+++ src/features/labels/lables-history.tsx
@@ -3,15 +3,13 @@ import { selectHistoryForProduction, useStoreCountLabel } from "@/entites/labels
 import { useQueryPlace, useQueryProductions } from "@/entites/users";
 import { $setting } from "@/shared";
 import {
-	Item,
-	ItemLabel,
-	ItemSection,
-	List,
+	DataColumn,
 	Loading,
+	TableData,
 	Text
 } from "@/shared/ui";
 import { factorySelect, type factorySelectProps } from "@/shared/utils";
-import { Group, Stack } from "@mantine/core";
+import { Card, Group, SimpleGrid, Stack } from "@mantine/core";
 import "@style";
 import dayjs from "dayjs";
 import { useEffect, useMemo, useState } from "react";
@@ -57,25 +55,41 @@ export const LabelsHistory = () => {
 					<SelectPrint label='Этикетка' value={print} onChange={setPrint} />
 				</Group>
 			</Group>
-			<List dense separator>
-				{data.map((item) => (
-					<Item key={item.id}>
-						<ItemSection left>
-							<ItemLabel>{item.place_name}</ItemLabel>
-							<ItemLabel caption>
-								{dayjs(item.date_applic).format($setting.get('formatDateTime'))} -{" "}
-								{item.format_template}
-							</ItemLabel>
-						</ItemSection>
-						<ItemSection side>
-							{item.consumption_m || "-"} м.
-						</ItemSection>
-						<ItemSection side className="!w-20">
-							{item.count_label} шт.
-						</ItemSection>
-					</Item>
-				))}
-			</List>
+			<TableData<ICountLabelHistoryItem> data={data} withHeader={false} breakpoint="xs" layout={({nodes, columns}) => {
+					return <SimpleGrid cols={1}>
+						{nodes.map((item) => <Card key={item.index} withBorder>
+							{columns.filter(column => column.isField).map((column) => <Group key={column.field as string} align='flex-start' justify='space-between' grow style={{
+								borderBottom: '1px dashed var(--mantine-color-default-border)',
+							}}>
+								<div>
+									<Text truncate="end">{item.data.place_name}</Text>
+									<Text opacity={0.6} fz='xs'>
+										{dayjs(item.data.date_applic).format($setting.get('formatDateTime'))} -{" "}
+										{item.data.format_template}
+									</Text>
+								</div>
+								<Stack>
+									<Text fw={500} >
+										{item.data.consumption_m || "-"} м.
+									</Text>
+									<Text fw={500} >
+										{item.data.count_label || "-"} шт.
+									</Text>
+								</Stack>
+							</Group>)}
+						</Card>)}
+					</SimpleGrid>
+				}}>
+				<DataColumn<ICountLabelHistoryItem> field="place_name" body={(item) => <>
+					<Text truncate="end">{item.place_name}</Text>
+					<Text opacity={0.6} fz='xs'>
+						{dayjs(item.date_applic).format($setting.get('formatDateTime'))} -{" "}
+						{item.format_template}
+					</Text>
+				</>} />
+				<DataColumn<ICountLabelHistoryItem> field="consumption_m" body={(item) => <>{item.consumption_m || "-"} м.</>} />
+				<DataColumn<ICountLabelHistoryItem> field="count_label" body={(item) => <>{item.count_label} шт.</>} />
+			</TableData>
 		</Stack>
 	</Loading>
 };
